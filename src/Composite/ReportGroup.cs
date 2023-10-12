0b857b9d96f70bfd3bfc2f130776996f84dc2858
takuma-kondo <takuma.kondo.j5m@jpgr.denso.com>
2023-10-12 17:04:10 +0900

    latest更新

---
+++ src/Composite/ReportGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace Composite;
+
+public class ReportGroup:ReportEntory
+{
+    public List<ReportEntory> Children { get; set; } = new List<ReportEntory>();
+
+
+    public  void AddChild(ReportEntory reportEntory)
+    {
+        Children.Add(reportEntory);
+    }
+
+
+    public override void Remove()
+    {
+        throw new NotImplementedException();
+    }
+}
