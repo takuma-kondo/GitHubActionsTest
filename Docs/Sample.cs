5d6d53620ad60ffa8c27c9787c880c33f1970692
takuma-kondo <takuma.kondo.j5m@jpgr.denso.com>
2023-11-29 09:57:54 +0900

    sample

---
+++ Docs/Sample.cs
@@ -0,0 +1,12 @@
+public class SampleClass
+{
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public string ConcatenateStrings(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+}
