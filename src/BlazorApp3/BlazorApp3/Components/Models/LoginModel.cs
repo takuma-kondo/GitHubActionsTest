495d3de448fe56d319d38cdcf6045c8a1610f98f
takuma-kondo <takuma.kondo.j5m@jpgr.denso.com>
2025-03-12 17:24:53 +0900

    抽選アプリ格納

---
+++ src/BlazorApp3/BlazorApp3/Components/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp3.Components.Models
+{
+    public class LoginModel
+    {
+        public int EmployeeNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/BlazorApp3/BlazorApp3/Components/Models/WinnerRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp3.Components.Models
+{
+    public class WinnerRecord
+    {
+
+        public int EmployeeNumber { get; set; }
+        public string Name { get; set; }
+        public DateTime WinningTime { get; set; }
+    }
+}
