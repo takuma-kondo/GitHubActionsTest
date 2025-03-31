2201f92ac2137b4eb60178c7ad2baecfb6fc90d3
takuma-kondo <takuma.kondo.j5m@jpgr.denso.com>
2025-03-31 13:22:08 +0900

    抽選アプリ格納

--- src/BlazorApp3/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ src/BlazorApp3/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708091bc672121f841c22c9879e84b00b3cf3798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495d3de448fe56d319d38cdcf6045c8a1610f98f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
